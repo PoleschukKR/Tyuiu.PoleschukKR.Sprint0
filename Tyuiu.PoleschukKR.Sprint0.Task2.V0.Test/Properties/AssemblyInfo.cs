405e966ba5943cfba63d05c2929bde07dca51b22
Tyuiu.PoleschukKR@outlook.com <kirillpolesuk05@gmail.com>
2023-09-06 16:40:28 +0500

    Task2

---
+++ Tyuiu.PoleschukKR.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PoleschukKR.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PoleschukKR.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("05a23a6b-6eec-4dd9-b06d-0114efb95fb8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PoleschukKR.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PoleschukKR.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.PoleschukKR.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вызов класса DataService и метода GetMessage
+            // Из библиотеки Tyuiu.PoleschukKR.Sprint0.Task2.V0.Lib
+            Console.WriteLine(DataService.GetMessage("Кирилл"));
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
