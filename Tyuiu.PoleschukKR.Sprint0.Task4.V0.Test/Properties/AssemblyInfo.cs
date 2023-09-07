3614eb73d1e255817e5957064d3dda1632f119be
Tyuiu.PoleschukKR@outlook.com <kirillpolesuk05@gmail.com>
2023-09-07 12:56:29 +0500

    Task4

---
+++ Tyuiu.PoleschukKR.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PoleschukKR.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PoleschukKR.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d5f9e5d2-7af3-4e77-975c-715ad675f308")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PoleschukKR.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PoleschukKR.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.PoleschukKR.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Пример линейной структуры программы
+            //Вызов метода сложения Addition
+            Console.WriteLine(DataService.Addition(1, 5));
+            //Вызов метода вычитания Substruction
+            Console.WriteLine(DataService.Substruction(15,5));
+            // Вызов метода умножения Multiplication
+            Console.WriteLine(DataService.Multiplication(10, 10));
+            //Вызов метода деления Division
+            Console.WriteLine(DataService.Division(5, 5));
+
+            Console.ReadKey();
+        }
+    }
+}
