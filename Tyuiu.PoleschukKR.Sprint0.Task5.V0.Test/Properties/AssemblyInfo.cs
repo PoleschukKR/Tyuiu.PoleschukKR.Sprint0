e85d14e9179237389a72513d2254b25001010c7f
Tyuiu.PoleschukKR@outlook.com <kirillpolesuk05@gmail.com>
2023-09-08 16:30:18 +0500

    Task5

---
+++ Tyuiu.PoleschukKR.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PoleschukKR.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PoleschukKR.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fb0a2f87-0649-42b3-b758-e5af5b758e22")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PoleschukKR.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PoleschukKR.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.PoleschukKR.Sprint0.Task5.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Пример линейной структуры 
+            Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+            Console.WriteLine("A - B = " + DataService.Substruction (15, 5));
+            Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+            //Пример разветвляющей структуры находится в библиотеке классов методов Division
+            Console.WriteLine("A / B = " + DataService.Division(16, 4));
+            Console.ReadKey();
+
+        }
+    }
+}
